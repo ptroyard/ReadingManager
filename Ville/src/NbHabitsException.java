d944db550178fa7274ff4943ebf0b65eb90d01fa
ptroyard <pierre.troyard@gmail.com>
2015-01-05 19:32:52 +0100

    RAZ

--- Ville/src/NbHabitsException.java
+++
@@ -1,9 +0,0 @@
-//Cependant, jamais créer ses propres exception... Galere pour reutilisation. Utiliser les coonues (ici numberformatexception)
-// Ainsi ca évite ici d'appeller super, et même de déclarer la classe !!!
-public class NbHabitsException extends Exception {
-
-	public NbHabitsException(String pMessage)
-	{
-		super(pMessage);
-	}
-}
